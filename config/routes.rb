bc2b2576d8db90a652bf6435bd4042bf43974e1a
Brian Holdman <bholdman@gmail.com>
2014-06-18 07:56:27 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ ShuttleApi::Application.routes.draw do
     resources :route_stops
     resources :gps_data
 
+    root 'routes#index'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
