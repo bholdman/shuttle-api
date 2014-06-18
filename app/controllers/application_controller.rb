146d9ff802ee6a02f7ba462ab4ca3ae2705fe515
Brian Holdman <bholdman@gmail.com>
2014-06-18 10:44:03 -0500

    added create method to routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,2 @@
-class ApplicationController < ActionController::API
-
+class ApplicationController < ActionController::BASE #had to change to Base from API to get protect_from_forgery to work in the routes controller
 end
--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,14 @@
 class RoutesController < ApplicationController
 	include ActionController::MimeResponds
+	protect_from_forgery with: :null_session
 	
+	def route_params
+		params.require(:route).permit(:name, :active)	
+	end
+
+	private :route_params
+
+
 	def index
 		routes = Route.all
 		if active = params[:active]
@@ -15,5 +23,12 @@ class RoutesController < ApplicationController
 	def show
 		route = Route.find_by_name(params[:id])
 		render json: route, status: 200
+	end
+
+	def create
+		route = Routes.new(route_params)
+		if route.save
+			render json: route, status: 201, location: route
+		end
 	end
 end
